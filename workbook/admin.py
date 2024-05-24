59ba780903e72eacca932cc3789c76d4fda470f4
hw21a078 <118120717+hw21a078@users.noreply.github.com>
2024-05-24 15:37:39 +0900

    Add

--- workbook/admin.py
+++ workbook/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import SpreadsheetData
 
-# Register your models here.
+class SpreadsheetDataAdmin(admin.ModelAdmin):
+    list_display = ("question_id", "question_number") # レコード一覧に問題IDと問題番号を表示する
+    list_display_links = ("question_id", "question_number") # 問題IDと問題番号にリンクを設定する
+    
+admin.site.register(SpreadsheetData, SpreadsheetDataAdmin) # djangoの管理サイトにモデルを登録し、SpreadsheetDataAdminを適用する
